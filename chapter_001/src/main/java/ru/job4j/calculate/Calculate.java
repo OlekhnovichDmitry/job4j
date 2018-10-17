b92366eac9b146df6d1cb9079199463c577c35e5
OlekhnovichDmitry <wride@yandex.ru>
2018-10-17 23:58:33 +0300

    1.3. JavaDoc. Code convention[#75637]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Вывод строки в консоль
+* @param args - args.
+* @autor Dmitry Olekhnovich (wride@yandex.ru)
+*/
+
+
 public class Calculate{
+	
+	/**
+	* Metod SOUP
+	* @return Hello world.
+	*/
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+﻿/**
+* Package for calculate task.
+*
+* @author Dmitry Olekhnovich
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;

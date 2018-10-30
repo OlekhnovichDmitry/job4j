2416e961291139fc664bd29ed4d4f1132c404ce8
OlekhnovichDmitry <wride@yandex.ru>
2018-10-30 21:22:12 +0300

    4.4. Максимум из трех чисел[#75603] вызов метода в методе.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -24,6 +24,6 @@ public class Max {
      * @return Вызов метода в методе.
      */
     public int maxThree(int first, int second, int third) {
-    return max(max(first, second), max(second, third));
+            return max(max(first, second), third);
     }
 }

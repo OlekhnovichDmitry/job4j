0b72ed1cd06613575608915b37c6027acb25b443
OlekhnovichDmitry <wride@yandex.ru>
2018-10-31 23:09:55 +0300

    6.2. Перевернуть массив.[#75623]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+/**
+ * работа с массивом. Переворачиваем массив.
+ *
+ * @author Dmitry Olekhnovich.
+ * @version $Id$
+ * @since 0.1
+ */
+public class Turn {
+	public int[] back(int[] array) {
+		for (int index = 0; index != array.length / 2; index++) {
+			int temp = array[index];
+			array[index] = array[array.length - index - 1];
+			array[array.length - index - 1] = temp;
+		}
+		return array;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test переворачивание массивов.
+ *
+ * @version $Id$
+ * @autor Dmitry Olekhnovich.
+ * @since 0.1
+ */
+public class TurnTest {
+	// переворачиваем четный массив
+	@Test
+	public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+		Turn turner = new Turn();
+		int[] input = new int[] {4, 1, 6, 2};
+		int[] result = turner.back(input);
+		int[] expect = new int[] {2, 6, 1, 4};
+		assertThat(result, is(expect));
+	}
+	//переворачиваем нечетный массив
+	@Test 
+	public void whenTurnArrayWithOddAmountOfElementThenTurnedArray() {
+
+		Turn turner = new Turn();
+		int[] input = new int[] {1, 2, 3, 4, 5};
+		int[] result = turner.back(input);
+		int[] expect = new int[] {5, 4, 3, 2, 1};
+		assertThat(result, is(expect));
+	}
+}

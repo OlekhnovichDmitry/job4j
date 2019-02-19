c8f302486291153cb645f3f4f39f22b1c229bef3
OlekhnovichDmitry <wride@yandex.ru>
2019-02-19 22:57:27 +0300

    1.Стажер / Часть 002. ООП / 4. Полиморфизм   4. Используя шаблон проектирования

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -0,0 +1,17 @@
+package ru.job4j.pseudo;
+
+/**
+ * @autor Dmitry Olekhnovich
+ * @version $id$
+ * @since 0.1
+ */
+public class Triangle implements Shape {
+    @Override
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("  +  ");
+        pic.append(" + + ");
+        pic.append("+++++");
+        return pic.toString();
+    }
+}
